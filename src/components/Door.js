b0972d5ece91d7950db5d4b00fdd9d71a8e5dfa2
Paulo Remoli <paulo002@gmail.com>
2022-03-21 23:49:53 +0100

    Add fog and door textures

--- src/components/Door.js
+++ src/components/Door.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import { useLoader } from '@react-three/fiber'
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import * as THREE from 'three';
 
 export default function Door(props) {
+  const doorColorTexture = useLoader(TextureLoader, '/textures/door/color.jpg');
+  const doorAlphaTexture = useLoader(TextureLoader, ' /textures/door/alpha.jpg');
+  const doorAmbientOcclusionTexture = useLoader(TextureLoader, ' /textures/door/ambientOcclusion.jpg');
+  const doorHeightTexture = useLoader(TextureLoader, ' /textures/door/height.jpg');
+  const doorNormalTexture = useLoader(TextureLoader, ' /textures/door/normal.jpg');
+  const doorMetalnessTexture = useLoader(TextureLoader, ' /textures/door/metalness.jpg');
+  const doorRoughnessTexture = useLoader(TextureLoader, ' /textures/door/roughness.jpg');
+
+  const geomRef = useRef()
+
+  React.useLayoutEffect(() => {
+     if(geomRef.current){
+       geomRef.current.setAttribute("uv2", new THREE.BufferAttribute(geomRef.current.attributes.uv.array, 2))
+     }
+  }, [])
+
+
   return (
-    <mesh
-      {...props} position={[0, 1, 2.01]}>
-      <planeGeometry args={[2, 2]}  />
-      <meshStandardMaterial color={'#a9c388'} />
+    <mesh 
+      {...props} position={[0, 0.9, 2.01]}>
+      <planeGeometry ref={geomRef} args={[2, 2]} />
+      <meshStandardMaterial map={doorColorTexture} transparent alphaMap={doorAlphaTexture} aoMap={doorAmbientOcclusionTexture} displacementMap={doorHeightTexture} displacementScale={0.1} normalMap={doorNormalTexture} metalnessMap={doorMetalnessTexture} roughnessMap={doorRoughnessTexture} />
     </mesh>
   )
 }
--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import { useThree } from '@react-three/fiber';
 import Plane from './Plane';
@@ -14,15 +14,18 @@ const Scene = () => {
 
   return (
     <>
-      <ambientLight color={"#b9d5ff"} intensity={0.12} />
-      <directionalLight color={"#b9d5ff"} intensity={0.12} position={[4, 5, -2]} />
-      <Plane position={[0, 0, 0]} />
-      <House />
-      <Bushes/>
-      <Graves/>
-      <OrbitControls enableDamping />
-      <PerspectiveCamera makeDefault position={[4, 2, 5]} fov={75} aspect={size.width / size.height} near={0.1} far={100} />
-      <fog args={["#ff0000", 2, 15]}/>
+
+      <Suspense fallback={null}>
+        <ambientLight color={"#b9d5ff"} intensity={0.12} />
+        <directionalLight color={"#b9d5ff"} intensity={0.12} position={[4, 5, -2]} />
+        <Plane position={[0, 0, 0]} />
+        <House />
+        <Bushes />
+        <Graves />
+        <OrbitControls enableDamping />
+        <PerspectiveCamera makeDefault position={[4, 2, 5]} fov={75} aspect={size.width / size.height} near={0.1} far={100} />
+        <fog args={["#262837", 1, 15]} attach="fog" />
+      </Suspense>
     </>
   );
 }
