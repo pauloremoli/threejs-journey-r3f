fffd2358cd13ce789d0d607388de6ab1809a4db2
Paulo Remoli <paulo002@gmail.com>
2022-03-24 20:15:42 +0100

    Galaxy generator

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-
-import { Canvas } from '@react-three/fiber';
-import React from 'react';
-import './App.css';
-import Scene from './components/Scene';
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import "./App.css";
+import Scene from "./components/Scene";
 
 export default function App() {
   return (
-    <Canvas className='canvas'>
-      <Scene />
-    </Canvas>
-  )
+    <>
+      <Canvas>
+        <Scene />
+      </Canvas>
+    </>
+  );
 }
-
--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,18 +1,30 @@
-import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
-import { useThree } from '@react-three/fiber';
-import React from 'react';
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
+import React from "react";
+import Particles from "./Particles";
 
 const Scene = () => {
   const { size } = useThree();
   return (
     <>
       <ambientLight color={"#ffffff"} intensity={0.5} />
-      <directionalLight color={"#ffffff"} intensity={0.5} position={[4, 5, -2]} />
+      <directionalLight
+        color={"#ffffff"}
+        intensity={0.5}
+        position={[4, 5, -2]}
+      />
       <OrbitControls enableDamping />
-      <PerspectiveCamera makeDefault position={[4, 2, 5]} fov={75} aspect={size.width / size.height} near={0.1} far={100} />
-
+      <PerspectiveCamera
+        makeDefault
+        position={[4, 2, 5]}
+        fov={75}
+        aspect={size.width / size.height}
+        near={0.1}
+        far={100}
+      />
+      <Particles />
     </>
   );
-}
+};
 
 export default Scene;
