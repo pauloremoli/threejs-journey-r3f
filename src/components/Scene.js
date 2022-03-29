d267e08122572297c2d7fb89f1d08d4c8d80d493
Paulo Remoli <paulo002@gmail.com>
2022-03-30 00:36:56 +0200

    Animated model

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,18 +1,33 @@
-import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
-import { useThree } from '@react-three/fiber';
-import React from 'react';
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
+import React, { Suspense } from "react";
+import Fox from "./Fox";
 
 const Scene = () => {
   const { size } = useThree();
   return (
     <>
       <ambientLight color={"#ffffff"} intensity={0.5} />
-      <directionalLight color={"#ffffff"} intensity={0.5} position={[4, 5, -2]} />
+      <directionalLight
+        color={"#ffffff"}
+        intensity={0.5}
+        position={[4, 5, -2]}
+      />
       <OrbitControls enableDamping />
-      <PerspectiveCamera makeDefault position={[4, 2, 5]} fov={75} aspect={size.width / size.height} near={0.1} far={100} />
+      <PerspectiveCamera
+        makeDefault
+        position={[4, 2, 5]}
+        fov={75}
+        aspect={size.width / size.height}
+        near={0.1}
+        far={100}
+      />
 
+      <Suspense fallback={null}>
+        <Fox />
+      </Suspense>
     </>
   );
-}
+};
 
 export default Scene;
