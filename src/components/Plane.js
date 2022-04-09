c965e6e79729409aac5f5b526ef1b8e4cb47364b
Paulo Remoli <paulo002@gmail.com>
2022-04-10 01:13:58 +0200

    basic shader

---
+++ src/components/Plane.js
@@ -0,0 +1,50 @@
+import { useMemo, useRef } from "react";
+import { useFrame } from "react-three-fiber";
+import { Vector2 } from "three";
+
+const vertexShader = `
+    uniform mat4 projectionMatrix;
+    uniform mat4 viewMatrix;
+    uniform mat4 modelMatrix;
+    attribute vec3 position;
+    uniform vec2 uFrequency;
+
+    void main() {
+      vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+      modelPosition.z += sin(modelPosition.x * uFrequency.x ) * 0.1;
+      modelPosition.z += sin(modelPosition.y * uFrequency.y ) * 0.1;
+      gl_Position = projectionMatrix * viewMatrix * modelPosition;
+    }
+`;
+
+const fragmentShader = `
+    precision mediump float;
+    
+    void main() {
+      gl_FragColor = vec4(1.0, 0, 0, 1.0);
+    }
+`;
+
+const Plane = () => {
+  const ref = useRef();
+
+  const data = useMemo(
+    () => ({
+      fragmentShader,
+      vertexShader,
+      uniforms: {
+          uFrequency: { value: new Vector2(10, 5)}
+      }
+    }),
+    []
+  );
+
+  return (
+    <mesh ref={ref}>
+      <planeGeometry args={[1, 1, 32, 32]} />
+      <rawShaderMaterial attach="material" {...data} />
+    </mesh>
+  );
+};
+
+export default Plane;
--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,6 +1,7 @@
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import { useThree } from '@react-three/fiber';
 import React from 'react';
+import Plane from './Plane';
 
 const Scene = () => {
   const { size } = useThree();
@@ -9,8 +10,8 @@ const Scene = () => {
       <ambientLight color={"#ffffff"} intensity={0.5} />
       <directionalLight color={"#ffffff"} intensity={0.5} position={[4, 5, -2]} />
       <OrbitControls enableDamping />
-      <PerspectiveCamera makeDefault position={[4, 2, 5]} fov={75} aspect={size.width / size.height} near={0.1} far={100} />
-
+      <PerspectiveCamera makeDefault position={[0, 2, 5]} fov={75} aspect={size.width / size.height} near={0.1} far={100} />
+      <Plane/>
     </>
   );
 }
